50a95249bf2fdb7f7bc7f01cd0ec18233e18f80f
Jonathan Adams <jd.adams16@gmail.com>
2020-10-11 19:01:30 -0500

    Add interface definitions.

---
+++ src/interfaces/recipe.ts
@@ -0,0 +1,21 @@
+export interface BaseRecipe {
+    title: string;
+    author: string;
+    image?: string;
+    description?: string;
+}
+
+export interface NewRecipe extends BaseRecipe {
+    published: Date;
+    recipe: string;
+    updated?: Date;
+    url?: string
+}
+
+export interface Recipe extends NewRecipe {
+    id: string;
+}
+
+export interface SummaryRecipe extends BaseRecipe {
+    id: string;
+}
