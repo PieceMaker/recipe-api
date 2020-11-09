a51cd3ff151a596dda1d8b8a772faf1627412944
Jonathan Adams <jd.adams16@gmail.com>
2020-11-09 00:27:45 -0600

    Use static dates.

--- db/populate.js
+++ db/populate.js
@@ -8,7 +8,7 @@ db.recipes.insertMany(
             _id: ObjectId("5fa8a136a26e5309eeda546b"),
             title: "Family Recipe",
             author: "Your mom",
-            published: new Date(),
+            published: new Date("1776-07-04T12:00:00.000Z"),
             recipe: "Some markdown to be filled in later.",
             description: "A delectable recipe for a dish your mom always cooks."
         },
@@ -16,7 +16,7 @@ db.recipes.insertMany(
             _id: ObjectId("5fa8a511460dae6b34c2dee7"),
             title: "Popeye's Spicy Strips",
             author: "Popeye",
-            published: new Date(),
+            published: new Date("1972-06-12T12:00:00.000Z"),
             recipe: "Ingredients and procedures for cooking Popeye's strips.",
             description: "How to cook the world's best spicy chicken strips."
         }
