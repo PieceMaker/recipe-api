7a8a0426ab9285f48d7f0bcff4ace9d5145fc8eb
Jonathan Adams <jd.adams16@gmail.com>
2020-11-14 14:03:34 -0600

    Pull recipes and identifiers into common constant variables.

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -2,24 +2,25 @@ import axios from 'axios';
 import { expect } from 'chai';
 import { recipes } from '../db/data/recipes';
 
+const mothersId = '5fa8a136a26e5309eeda546b';
+const popeyesId = '5fa8a511460dae6b34c2dee7';
+const mothersRecipe = recipes.find(recipe => recipe._id === mothersId);
+const popeyesRecipe = recipes.find(recipe => recipe._id === popeyesId);
+
 describe('API', function() {
 
     describe('Load', function() {
 
         it('should load mother\'s recipe given the mother\'s recipe id', async function() {
-            const mothersId = '5fa8a136a26e5309eeda546b';
-            const recipe = recipes.find(recipe => recipe._id === mothersId);
             const apiRecipe = await axios.get(`http://localhost:8000/load/${mothersId}`)
                 .then(({ data }) => data);
-            expect(apiRecipe).to.eql(recipe);
+            expect(apiRecipe).to.eql(mothersRecipe);
         });
 
         it('should load Popeye\'s recipe given the Popeye\'s recipe id', async function() {
-            const popeyesId = '5fa8a511460dae6b34c2dee7';
-            const recipe = recipes.find(recipe => recipe._id === popeyesId);
             const apiRecipe = await axios.get(`http://localhost:8000/load/${popeyesId}`)
                 .then(({ data }) => data);
-            expect(apiRecipe).to.eql(recipe);
+            expect(apiRecipe).to.eql(popeyesRecipe);
         });
 
     });
