3a6d95a770b4c59c545484801354c7184db2a824
Jonathan Adams <jd.adams16@gmail.com>
2020-11-14 15:30:50 -0600

    Pull axios requests into functions.

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { expect } from 'chai';
 import { recipes } from '../db/data/recipes';
-import { MongoRecipe, SearchResult } from "../src/types/recipe";
+import {MongoRecipe, Recipe, SearchResult} from "../src/types/recipe";
 import { integer } from "../src/types/integer";
 
 const mothersId = '5fa8a136a26e5309eeda546b';
@@ -16,20 +16,26 @@ const sortById = function(recipe1: MongoRecipe, recipe2: MongoRecipe): integer {
         return 1;
     return 0;
 }
+const apiLoad = function (id: string): Promise<Recipe> {
+    return axios.get(`http://localhost:8000/load/${id}`)
+        .then(({ data }) => data);
+}
+const apiSearch = function(searchTerm: string | undefined): Promise<SearchResult> {
+    return axios.get<SearchResult>(`http://localhost:8000/search/${searchTerm}`)
+        .then(({ data }) => data);
+}
 
 describe('API', function() {
 
     describe('Load', function() {
 
         it('should load mother\'s recipe given the mother\'s recipe id', async function() {
-            const apiRecipe = await axios.get(`http://localhost:8000/load/${mothersId}`)
-                .then(({ data }) => data);
+            const apiRecipe = await apiLoad(mothersId);
             expect(apiRecipe).to.eql(mothersRecipe);
         });
 
         it('should load Popeye\'s recipe given the Popeye\'s recipe id', async function() {
-            const apiRecipe = await axios.get(`http://localhost:8000/load/${popeyesId}`)
-                .then(({ data }) => data);
+            const apiRecipe = await apiLoad(popeyesId);
             expect(apiRecipe).to.eql(popeyesRecipe);
         });
 
@@ -38,39 +44,34 @@ describe('API', function() {
     describe('Search', function() {
 
         it('should return both a count and an array of recipes', async function() {
-            const searchResult = await axios.get('http://localhost:8000/search/a')
-                .then(({ data }) => data);
+            const searchResult = await apiSearch('a');
             expect(searchResult).to.have.keys([ 'count', 'recipes' ]);
         });
 
         it('should search by title', async function() {
             const title = mothersRecipe?.title;
-            const searchResult = await axios.get<SearchResult>(`http://localhost:8000/search/${title}`)
-                .then(({ data }) => data);
+            const searchResult = await apiSearch(title);
             expect(searchResult.count).to.equal(1);
             expect(searchResult.recipes).to.eql([ mothersRecipe ]);
         });
 
         it('should search by description', async function() {
             const description = popeyesRecipe?.description;
-            const searchResult = await axios.get<SearchResult>(`http://localhost:8000/search/${description}`)
-                .then(({ data }) => data);
+            const searchResult = await apiSearch(description);
             expect(searchResult.count).to.equal(1);
             expect(searchResult.recipes).to.eql([ popeyesRecipe ]);
         });
 
         it('should search by recipe', async function() {
            const recipe = popeyesRecipe?.recipe;
-           const searchResult = await axios.get<SearchResult>(`http://localhost:8000/search/${recipe}`)
-               .then(({ data }) => data);
+           const searchResult = await apiSearch(recipe);
            expect(searchResult.count).to.equal(1);
            expect(searchResult.recipes).to.eql([ popeyesRecipe ]);
         });
 
         it('should search using "like" comparison', async function() {
             const commonSearchTerm = 'a';
-            const searchResult = await axios.get<SearchResult>(`http://localhost:8000/search/${commonSearchTerm}`)
-                .then(({ data }) => data);
+            const searchResult = await apiSearch(commonSearchTerm);
             expect(searchResult.count).to.equal(recipes.length);
             const sortedRecipes = recipes.sort(sortById);
             const sortedAPIRecipes = recipes.sort(sortById);
