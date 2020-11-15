8a140e863bdd61b8fffa710ba1d180b54d83536c
Jonathan Adams <jd.adams16@gmail.com>
2020-11-14 19:33:24 -0600

    Add a bunch more sample recipes.

--- db/data/recipes.ts
+++ db/data/recipes.ts
@@ -16,5 +16,108 @@ export const recipes: MongoRecipe[] =  [
     published: "1972-06-12T12:00:00.000Z",
     recipe: "Ingredients and procedures for cooking Popeye's strips.",
     description: "How to cook the world's best spicy chicken strips."
+  },
+  {
+    _id: "5fb0754318c0a72e04331f96",
+    title: "Steak Pizzaiola",
+    author: "Marie Barone",
+    published: "1925-11-24T12:00:00.000Z",
+    recipe: "Steak and stuff",
+    description: "Steak dish commonly served in Everybody Loves Raymond."
+  },
+  {
+    _id: "5fb07613579f6de7d881be56",
+    title: "Chicken Fettuccine Alfredo",
+    author: "Food Network",
+    published: "2018-04-27T12:00:00.000Z",
+    recipe: "Chicken, fettuccine noodles, and alfredo sauce...",
+    url: "https://www.foodnetwork.com/recipes/food-network-kitchen/chicken-fettuccine-alfredo-3364118"
+  },
+  {
+    _id: "5fb0770e7dad2283a7ffd335",
+    title: "Butter Chicken",
+    author: "Anonymous",
+    published: "2018-08-12T12:00:00.000Z",
+    recipe: "It's a secret.",
+    description: "Heaven in your mouth."
+  },
+  {
+    _id: "5fb0778fe809db7536b3b67d",
+    title: "Stuffed Bell Peppers",
+    author: "Colleen Case",
+    published: "2034-01-05T12:00:00.000Z",
+    recipe: "Red bell peppers stuffed with pulled pork, rice, and cheese baked in the oven for 45 minutes.",
+    description: "Baked red bell peppers stuffed with filling"
+  },
+  {
+    _id: "5fb0788ab03235a70e65c4c0",
+    title: "Tacos",
+    author: "Maria Reina",
+    published: "2011-10-10T12:00:00.000Z",
+    recipe: "Ground beef, onions, garlic, tomato sauce, and Mexican spices cooked to perfection.",
+    description: "Tacos. 'Nuff said."
+  },
+  {
+    _id: "5fb079151ba2603ac52fb1a0",
+    title: "Noodles",
+    author: "Family Recipe",
+    published: "1952-12-25T12:00:00.000Z",
+    updated: "1960-12-25T12:00:00.000Z",
+    recipe: "Combine 1 large egg with 100 grams of semolina flour. Mix well and let set. Then press into parchment and cut to size.",
+    description: "Homemade egg noodles."
+  },
+  {
+    _id: "5fb080ec80ca03cc9b8ffce6",
+    title: "Cup o' Noodles",
+    author: "Maruchan",
+    published: "1999-03-01T12:00:00.000Z",
+    recipe: "Take a cup of noodles. Add boiling water. Wait three minutes. Eat.",
+    description: "Classic college meal."
+  },
+  {
+    _id: "5fb0810f2761a4d66c237ace",
+    title: "PB&J Sandwich",
+    author: "Unknown",
+    published: "0001-01-01T12:00:00.000Z",
+    recipe: "Let's face it. You know how to make one of these."
+  },
+  {
+    _id: "5fb08173f228f41f39684348",
+    title: "Chicken Noodle Soup",
+    author: "Unknown",
+    published: "0001-01-01T12:00:00.000Z",
+    recipe: "Chicken broth, vegetables, chicken, mixed together and simmered. Noodles added at end.",
+    description: "Classic sick food."
+  },
+  {
+    _id: "5fb08187cd229e1e143d2fdd",
+    title: "Boiled Egg",
+    author: "Unknown",
+    published: "0001-01-01T12:00:00.000Z",
+    recipe: "Place egg in boiling water. Boil for 15 minutes. Peel and serve."
+  },
+  {
+    _id: "5fb0819827bc7f05e213ad2a",
+    title: "Whataburger",
+    author: "Whataburger",
+    published: "1950-08-08T12:00:00.000Z",
+    recipe: "Drive to nearest Whataburger and order a #1.",
+    description: "Classic Texas drunk food."
+  },
+  {
+    _id: "5fb081ae0123d89b8d9d852f",
+    title: "Meatloaf",
+    author: "Unknown",
+    published: "1929-10-29T12:00:00.000Z",
+    recipe: "Ground beef and a lot of nasty stuff.",
+    description: "Nasty crap."
+  },
+  {
+    _id: "5fb081beb98cfe9f81d9f9a5",
+    title: "Margarita",
+    author: "Unknown",
+    published: "1836-03-02T12:00:00.000Z",
+    recipe: "A drink with a 3:2:1 ratio. Three parts good tequila, two parts orange liqueur, one part lime juice. Shake and strain.",
+    description: "A perfect drink to enjoy on a spring evening on the porch while watching storms in the distance."
   }
 ];
